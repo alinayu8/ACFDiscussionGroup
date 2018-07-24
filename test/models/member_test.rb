631e949871967a1ff6cc5db71bbc2e2330abe891
Alina Yu <alinayu8@yahoo.com>
2018-07-23 23:35:41 -0400

    Wrote member context tests

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,5 +1,38 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  
+    context "Creating context" do
+        setup do 
+            create_cellGroups
+            create_members
+        end
+        
+        teardown do
+            destroy_cellGroups
+            destroy_members
+        end
+      
+        should "parse through text of names" do
+            text = """
+            Christian, Beacon, male
+            Hanna, Kindle
+            Nick, male
+            """
+            Member.member_list(text)
+            assert equal Member.all.count, 13
+
+            bad_text = """
+            James, Axial, male
+            Jason
+            """
+            Member.member_list(bad_text)
+            assert equal Member.all.count, 11
+
+            worse_text = """
+            Annabelle
+            """
+            Member.member_list(worse_text)
+            assert equal Member.all.count, 10
+        end
+    end
 end
