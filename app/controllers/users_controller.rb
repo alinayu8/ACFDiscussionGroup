561b7bae5f82bb93f6bb7d2a53da3318a86e9e5d
Jennifer <jjhuang1@andrew.cmu.edu>
2018-08-23 19:31:54 -0700

    added tentative user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,55 @@
+# adapted from PATS v2 and Alina's IS project
 class UsersController < ApplicationController
-    def index
-    end
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :check_login
+	authorize_resource
 
-    def settings
-    end
+	def index 
+		@users = User.all.paginate(page: params[:page]).per_page(15)
+	end 
+
+	def new 
+		@user = User.new
+	end 
+
+	def edit
+		@user.role = "volunteer" if current_user.role?(:volunteer)
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.role = "volunteer" if current_user.role?(:volunteer)
+		if @user.save
+			flash[:notice] = "Successfully added #{@user.name} as a user"
+			redirect_to users_path
+		else 
+			render action: 'new'
+		end 
+	end 
+
+	def update
+	    if @user.update_attributes(user_params)
+	      flash[:notice] = "Successfully updated #{@user.first_name}."
+	      redirect_to users_url
+	    else
+	      render action: 'edit'
+	    end
+	 end
+
+	def destroy
+		if @user.destroy
+	        redirect_to users_url, notice: "Successfully removed #{@user.name}"
+    	else
+    		render action: 'show'
+    	end
+	end
+
+  	private
+    	def set_user
+      		@user = User.find(params[:id])
+    	end
+
+    	def user_params
+      		params.require(:user).permit(:username, :first_name, :last_name, :email, :role, :active, :password, :password_confirmation)
+    	end
 end
